be4c5ef6a4ad38c2c6ceabff87c897918f41b8be
andywijaya15 <andykevonata@gmail.com>
2022-09-06 13:44:20 +0700

    Add Post controller and view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //render view with posts
+        // compact adalah command php untuk mengirim data posts ke view
+        return view('posts.index', compact('posts'));
+    }
+}
