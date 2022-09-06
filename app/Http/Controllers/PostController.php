ac53bfafedbc9d1b2e9ff4931b3bcf4d86abf0bb
andywijaya15 <andykevonata@gmail.com>
2022-09-06 14:05:38 +0700

    Add Post Update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+// Menghapus Gambar dari Server
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -61,5 +63,63 @@ class PostController extends Controller
 
         //redirect to index
         return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+    /**
+     * edit
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    // Parameter Post $post untuk mengambil ID dari model Post
+    public function edit(Post $post)
+    {
+        // ketika sudah didapatkan kita kirim $post melalui compact ke view
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $post
+     * @return void
+     */
+    public function update(Request $request, Post $post)
+    {
+        //validate form
+        $this->validate($request, [
+            'image'     => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/' . $post->image);
+
+            //update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        } else {
+
+            //update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 }
